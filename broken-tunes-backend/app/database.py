f7e3be0b904a3c8ad81b17bd51a94a0b5fdadd5b
yuzuokogi18 <233351@ids.upchiapas.edu.mx>
2026-03-12 07:16:09 -0600

    Load database schema in CI

--- broken-tunes-backend/app/database.py
+++ broken-tunes-backend/app/database.py
@@ -1,18 +1,18 @@
 import os
 import mysql.connector
 from mysql.connector.connection import MySQLConnection
-
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
 def get_db() -> MySQLConnection:
     try:
-      return mysql.connector.connect(
-            host=os.getenv('DB_HOST', '127.0.0.1'),
-            user=os.getenv('DB_USER', 'root'),
-            password=os.getenv('DB_PASS', '12345'),
-            database=os.getenv('DB_NAME', 'broken_tunes2')
+        return mysql.connector.connect(
+            host=os.getenv("DB_HOST", "127.0.0.1"),
+            user=os.getenv("DB_USER", "root"),
+            password=os.getenv("DB_PASS", "root"),
+            database=os.getenv("DB_NAME", "broken_tunes")
         )
     except mysql.connector.Error as e:
         print(f"Error connecting to MySQL: {e}")
